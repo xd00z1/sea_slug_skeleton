7b86b63780a321ecd433c6e830e8879504fb2d7d
Marianne Bezaire <mariannejcase@gmail.com>
2021-07-13 13:07:50 -0400

    Plot moved to fcn
    Placed plotting commands into a function so as not to interfere with Gradescope

--- skeleton_habituationGWR.py
+++ skeleton_habituationGWR.py
@@ -47,21 +47,27 @@ y = np.zeros((1,nTs)) # set up (define) a vector for the output time series
 ###############################
 # Plot the results
 ###############################
+def showresults(x,y,nTs,stv):
+    # Plot both the input series (vector x)
+    # and the resulting output series (vector y)
+    fig = plt.figure()
+    ax1 = fig.add_subplot(211)
+    ax1.plot(x) #, color='white',  antialiased=False, edgecolors='black', linewidth=1, shade=False, alpha=1)
+    ax1.set_ylabel('Input')
+    ax1.set_xlim(0, nTs)
+    ax1.set_ylim(0, 1.1)
+    
+    ax2 = fig.add_subplot(212)
+    ax2.plot(y[0]) #, color='white',  antialiased=False, edgecolors='black', linewidth=1, shade=False, alpha=1)
+    ax2.set_xlabel('Time step')
+    ax2.set_ylabel('Output')
+    ax2.set_xlim(0, nTs)
+    ax2.set_ylim(0, stv+0.5)
+    
+    plt.show()
 
-# Plot both the input series (vector x)
-# and the resulting output series (vector y)
-fig = plt.figure()
-ax1 = fig.add_subplot(211)
-ax1.plot(x) #, color='white',  antialiased=False, edgecolors='black', linewidth=1, shade=False, alpha=1)
-ax1.set_ylabel('Input')
-ax1.set_xlim(0, nTs)
-ax1.set_ylim(0, 1.1)
-
-ax2 = fig.add_subplot(212)
-ax2.plot(y[0]) #, color='white',  antialiased=False, edgecolors='black', linewidth=1, shade=False, alpha=1)
-ax2.set_xlabel('Time step')
-ax2.set_ylabel('Output')
-ax2.set_xlim(0, nTs)
-ax2.set_ylim(0, stv+0.5)
-
-plt.show()
+# TODO prepare file for Gradescope before committing and submitting!
+# - Comment out the call to showresults below
+# - Comment out any print statements added during code development
+# - Make sure you keep x, y, pls, and other variable names the same
+showresults(x,y,nTs,stv)
