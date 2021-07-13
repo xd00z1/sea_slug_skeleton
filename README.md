a5a52e57ea9f0b7955228fb5fe3623d20932ba6f
Marianne Bezaire <mariannejcase@gmail.com>
2021-07-13 11:59:22 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# sea_slug_skeleton
+ Skeleton program file in which to create a simulation of the sea slug gill reflex.
---
+++ skeleton_habituationGWR.py
@@ -0,0 +1,67 @@
+# Based on Tom Anastasio's habituationGWR.m
+# this script sets up a very simple simulation of habituation
+# of the Aplysia gill withdrawal reflex
+#
+# Modified for BU's RISE Practicum Comp Neuro lab by mbezaire@bu.edu
+
+###############################
+# import libraries with special
+# functions needed in this script
+###############################
+import numpy as np
+import matplotlib.pyplot as plt
+
+###############################
+# Define input stimulation
+###############################
+# TODO: Set a variable called stv to 4, this will define 
+#        the weight of the connection from input to output
+# stv = 
+
+# TODO: set up an input pulse called pls
+# pls = 
+
+# TODO: then create a list of 6 pulses, called x, to use for input
+# x = 
+
+v = stv # Set connection weight to start weight value
+
+###############################
+# Set up and run simulation
+###############################
+
+nTs = len(x) # find the length of the input list
+y = np.zeros((1,nTs)) # set up (define) a vector for the output time series
+
+# TODO: use a for-loop to iterate 
+#        through each time step in 
+#        the input series and calculate
+#        the output at each time step. Ex:
+# for ...
+#     then indent 4 spaces and write the equation that
+#     describes how each input value in the vector x is 
+#     transformed to the output value in the vector y
+
+
+
+###############################
+# Plot the results
+###############################
+
+# Plot both the input series (vector x)
+# and the resulting output series (vector y)
+fig = plt.figure()
+ax1 = fig.add_subplot(211)
+ax1.plot(x) #, color='white',  antialiased=False, edgecolors='black', linewidth=1, shade=False, alpha=1)
+ax1.set_ylabel('Input')
+ax1.set_xlim(0, nTs)
+ax1.set_ylim(0, 1.1)
+
+ax2 = fig.add_subplot(212)
+ax2.plot(y[0]) #, color='white',  antialiased=False, edgecolors='black', linewidth=1, shade=False, alpha=1)
+ax2.set_xlabel('Time step')
+ax2.set_ylabel('Output')
+ax2.set_xlim(0, nTs)
+ax2.set_ylim(0, stv+0.5)
+
+plt.show()
